3760226a1b1534f490b6b5e23f3d7cf0848f5296
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-28 21:24:20 +0530

    tailwind_practices : complete colors in tailwind css

--- tailwind-prac/src/App.jsx
+++ tailwind-prac/src/App.jsx
@@ -1,7 +1,18 @@
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Tailwind is working!</h1>
+    <div className="">
+
+    {/* ---------------------  colors --------------- */}
+      <h1 className="bg-indigo-600 text-amber-50">Colors1</h1>
+      <p className="bg-sky-700/50">Colors 2</p>
+      <p className="bg-slate-800 text-rose-300 dark:bg-rose-300 dark:text-slate-800">Colors 3 light and dark mode</p>
+      <p className="bg-midnight text-tahiti">Colors from custom</p>
+      <p className="bg-[#003049] text-[#FCBF49]">Colors 5</p>
+      {/* --- todo : @theme inline, component --- */}
+      <p> todo : .....</p>
+
+
+      
     </div>
   );
 }
