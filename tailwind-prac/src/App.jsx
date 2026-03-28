855c2d21d6bb622685077a8361fa616285719f10
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-28 21:44:11 +0530

    tailwind_practices : complete font-family in tailwind css

--- tailwind-prac/src/App.jsx
+++ tailwind-prac/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="">
 
-    {/* ---------------------  colors --------------- */}
+    {/* 1. ---------------------  colors --------------- */}
       <h1 className="bg-indigo-600 text-amber-50">Colors1</h1>
       <p className="bg-sky-700/50">Colors 2</p>
       <p className="bg-slate-800 text-rose-300 dark:bg-rose-300 dark:text-slate-800">Colors 3 light and dark mode</p>
@@ -10,9 +10,21 @@ function App() {
       <p className="bg-[#003049] text-[#FCBF49]">Colors 5</p>
       {/* --- todo : @theme inline, component --- */}
       <p> todo : .....</p>
+      <p> --------------------------------------------------------------</p>
+
+    {/*  2. -------------------- typography ----------------- */}
+        {/* 2.1 ------------ font familt ------------ */}
+        <h1 className="font-sans">font family sans</h1>
+        <h1 className="font-mono">font family mono</h1>
+        <p className="font-[Open-sans]">font open sans</p>
+        <p className="font-sans md:font-mono sm:font-serif">font responsive</p>
+        <p className="font-roboto">google font roboto</p>
+        <div className="font-display"> font for this entire div Lorem ipsum dolor sit amet.</div>
+        {/* ---- todo : custom font {@font-face} */}
+        <p>todo : .........</p>
+        <p>--------------------------------------------------------------</p>
 
 
-      
     </div>
   );
 }
