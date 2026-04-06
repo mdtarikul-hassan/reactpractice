a4d5abf2b95ac39ae7ddf0c1d47ad8b8bd339c1e
tarikul <mdtarikulhassan5432@gmail.com>
2026-04-06 23:21:04 +0530

    tailwind_practices : complete grid gap in tailwind css

--- tailwind-prac/src/App.jsx
+++ tailwind-prac/src/App.jsx
@@ -491,6 +491,18 @@ function App() {
           <div className="rounded-2xl w-14 bg-red-500">7</div>
         </div>
 
+        {/* 7.14 ---------- grid gap ----------- */}
+        <h1>Grid gap</h1>
+        <div className="grid grid-cols-3 gap-x-8 gap-y-4">
+          <div className="rounded-2xl w-14 bg-red-500">1</div>
+          <div className="rounded-2xl w-14 bg-red-500">2</div>
+          <div className="rounded-2xl w-14 bg-red-500">3</div>
+          <div className="rounded-2xl w-14 bg-red-500">4</div>
+          <div className="rounded-2xl w-14 bg-red-500">5</div>
+          <div className="rounded-2xl w-14 bg-red-500">6</div>
+          <div className="rounded-2xl w-14 bg-red-500">7</div>
+        </div>
+
 
 
 
