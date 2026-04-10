b11b51ae3e2a9f175618523d8c6b1ed422fe0d93
tarikul <mdtarikulhassan5432@gmail.com>
2026-04-10 23:31:25 +0530

    tailwind_practices : display in layout

--- tailwind-prac/src/App.jsx
+++ tailwind-prac/src/App.jsx
@@ -726,6 +726,23 @@ function App() {
           <h1>hello</h1>
         </div>
 
+        {/* 8.8 ------------ display ------------ */}
+        <h1>Display</h1>
+        <div>
+          <p>
+            When controlling the flow of text, using the CSS property <span class="inline">display: inline</span> will cause the
+            text inside the element to wrap normally.
+          </p>
+          <p>
+            While using the property <span class="inline-block">display: inline-block</span> will wrap the element to prevent the
+            text inside from extending beyond its parent.
+          </p>
+          <p>
+            Lastly, using the property <span class="block">display: block</span> will put the element on its own line and fill its
+            parent.
+          </p>
+        </div>
+
 
 
 
