78a3cd298fd1e7d84afab54037469b43a5fcf244
tarikul <mdtarikulhassan5432@gmail.com>
2026-04-17 23:21:29 +0530

    tailwind_practices : border collapse in table

--- tailwind-prac/src/App.jsx
+++ tailwind-prac/src/App.jsx
@@ -1008,6 +1008,34 @@ function App() {
             <img src="https://picsum.photos/500/300" className="backdrop-sepia rounded-lg"/>
           </div>
 
+        {/* 11 ----------------------------- table ------------------------------- */}
+          {/* 11.1 ------ border collapse ------------ */}
+          <div>
+            <h1>Border collapse</h1>
+            <table class="border-collapse border border-gray-400 bg-cyan-900 text-gray-300">
+              <thead>
+                <tr>
+                  <th class="border border-gray-300 bg-cyan-900 text-gray-300">State</th>
+                  <th class="border border-gray-300 bg-cyan-900 text-gray-300">City</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class="border border-gray-300 bg-cyan-900 text-gray-300">Indiana</td>
+                  <td class="border border-gray-300 bg-cyan-900 text-gray-300">Indianapolis</td>
+                </tr>
+                <tr>
+                  <td class="border border-gray-300 bg-cyan-900 text-gray-300">Ohio</td>
+                  <td class="border border-gray-300 bg-cyan-900 text-gray-300">Columbus</td>
+                </tr>
+                <tr>
+                  <td class="border border-gray-300 bg-cyan-900 text-gray-300">Michigan</td>
+                  <td class="border border-gray-300 bg-cyan-900 text-gray-300">Detroit</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
 
 
 
