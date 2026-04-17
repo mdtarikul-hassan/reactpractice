a32522acd5571181b78681b4bf9db959a87912db
tarikul <mdtarikulhassan5432@gmail.com>
2026-04-17 23:04:50 +0530

    my-app : react router with NavLink

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -15,7 +15,7 @@ import First from "./First"
 import Timer from "./Timer"
 import WindowSizeTracker from "./WindowSizeTracker"
 import Users from "./Users"
-import { BrowserRouter, Routes, Route, Link, useParams} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, useParams, NavLink} from 'react-router-dom'
 
 import Home from "./Home"
 import About from "./About"
@@ -70,7 +70,7 @@ function App() {
         {/* <a href="/">Home</a> | <a href="/about">About</a> | <a href="/contact">Contact</a> */}
         <nav>
           <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/contact">Contact</Link> | <Link to="/user/10">User</Link> | 
-          <Link to="/products">Products</Link>
+          <NavLink to="/products">Products</NavLink>
 
         </nav>
 
@@ -78,7 +78,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/user/:id" element={<User />} />
+          {/* <Route path="/user/:id" element={<User />} /> */}
           <Route path="/products" element={<Products />} >
             <Route path="phone" element={<Phone />} />
             <Route path="laptop" element={<Laptop />} />
