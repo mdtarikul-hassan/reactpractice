4c6c81bb6bb08415adb1b8a14bb1fcd6b1c850b5
tarikul <mdtarikulhassan5432@gmail.com>
2026-04-17 23:23:36 +0530

    tailwind_practices : table layout

--- tailwind-prac/src/App.jsx
+++ tailwind-prac/src/App.jsx
@@ -1063,6 +1063,34 @@ function App() {
             </table>
           </div>
 
+          {/* 11.3 ---------- table layout ------------ */}
+          <div>
+            <h1>table layout</h1>
+            <table class="table-auto border border-gray-400 bg-cyan-900 text-gray-300">
+              <thead>
+                <tr>
+                  <th class="border border-gray-300 bg-cyan-900 text-gray-300">State</th>
+                  <th class="border border-gray-300 bg-cyan-900 text-gray-300">City</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class="border border-gray-300 bg-cyan-900 text-gray-300">Indiana</td>
+                  <td class="border border-gray-300 bg-cyan-900 text-gray-300">Indianapolis</td>
+                </tr>
+                <tr>
+                  <td class="border border-gray-300 bg-cyan-900 text-gray-300">Ohio</td>
+                  <td class="border border-gray-300 bg-cyan-900 text-gray-300">Columbus</td>
+                </tr>
+                <tr>
+                  <td class="border border-gray-300 bg-cyan-900 text-gray-300">Michigan</td>
+                  <td class="border border-gray-300 bg-cyan-900 text-gray-300">Detroit</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+
 
 
     </div>
